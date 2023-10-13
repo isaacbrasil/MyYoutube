bf19968d59d3304d7b3ade6f573762f01c76376e
IsaacBrasil <isaac_brasil_oliveira@hotmail.com>
2023-10-13 14:42:53 -0300

    Inicializando

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/system";
+
+const ChannelDetail = () => {
+  return <div>ChannelDetail</div>;
+};
+
+export default ChannelDetail;
